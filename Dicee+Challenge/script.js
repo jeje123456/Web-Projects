64c01e71d2feb988435e8ac8b7698e0272d53831
pjh <[je377523@gmail.com]>
2021-12-22 11:18:30 +0900

    수업용선생님파일

--- Dicee+Challenge/script.js
+++ Dicee+Challenge/script.js
@@ -4,9 +4,6 @@
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
 
-img1.setAttribute('src', 'images/dice6.png');
-img2.setAttribute('src', 'images/dice6.png');
-
 //랜덤숫자를 만들어서 그 숫자값의 주사위를 출력한다.
 //변수 let (자바스크립트는 타입이 필요없다.)
 let n1 = Math.floor(Math.random() * 6) + 1;
@@ -14,3 +11,6 @@ let n2 = Math.floor(Math.random() * 6) + 1;
 //확인 : 콘솔에 출력
 console.log(n1);
 console.log(n2);
+
+img1.setAttribute('src', 'images/dice' + n1 + '.png');
+img2.setAttribute('src', 'images/dice' + n2 + '.png');
