206d3b1b5f22dcba1a19cdaaa3df94f39f5ec217
pjh <[je377523@gmail.com]>
2021-12-22 10:42:43 +0900

    수업용선생님파일

---
+++ Dicee+Challenge/script.js
@@ -0,0 +1,16 @@
+//alert('테스트')
+//이미지 태그
+//상수 const로 태그 객체를 저장한다.
+const img1 = document.querySelector('.img1');
+const img2 = document.querySelector('.img2');
+
+img1.setAttribute('src', 'images/dice6.png');
+img2.setAttribute('src', 'images/dice6.png');
+
+//랜덤숫자를 만들어서 그 숫자값의 주사위를 출력한다.
+//변수 let (자바스크립트는 타입이 필요없다.)
+let n1 = Math.floor(Math.random() * 6) + 1;
+let n2 = Math.floor(Math.random() * 6) + 1;
+//확인 : 콘솔에 출력
+console.log(n1);
+console.log(n2);
