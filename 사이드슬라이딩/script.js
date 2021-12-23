78ff3799a811bf2ef0a68ed90b3f774718c6ac23
pjh <[je377523@gmail.com]>
2021-12-23 11:37:19 +0900

    211223

--- 사이드슬라이딩/script.js
+++ 사이드슬라이딩/script.js
@@ -1,1 +1,19 @@
 // 확인용 alert('하이');
+// 클래스 panel들을 모두 선택해서 사우 panel에 담기
+// querySelectorAll : 여러개의 객체를 배열로 저장함
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  // panel.addEventListener('click', 함수);
+  //함수자리에 익명함수 사용
+  panel.addEventListener('click', () => {
+    removeActive();
+    panel.classList.add('active'); // 패널에 active 클래스를 추가
+  });
+});
+// 모든 패널에 active 클래스 제거
+function removeActive() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
