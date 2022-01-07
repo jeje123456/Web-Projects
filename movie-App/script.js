ffa1b08c55dbcbbb15e16f08bb36e7cd06a2cca0
pjh <[je377523@gmail.com]>
2022-01-07 14:07:48 +0900

    220107

--- movie-App/script.js
+++ movie-App/script.js
@@ -3,6 +3,11 @@ const API_URL =
   'https://api.themoviedb.org/3/discover/movie?api_key=e36d520a271fd7ce5f2d0714fa2ffe1b&language=ko&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
 // 포스터 이미지 앞의 주소
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+// 영화 검색
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=e36d520a271fd7ce5f2d0714fa2ffe1b&language=ko&query=';
+
 // 태그 객체 생성
 const main = document.getElementById('main'); // 영화가 들어가는 부분
 const form = document.getElementById('form');
@@ -39,7 +44,7 @@ function 영화보여주기(movies) {
     />
     <div class="movie-info">
       <h3>${title}</h3>
-      <span class="green">${vote_average}</span>
+      <span class="${평점별색상선택(vote_average)}">${vote_average}</span>
     </div>
     <div class="overview">
        <h3>상세보기</h3>
@@ -49,3 +54,31 @@ function 영화보여주기(movies) {
     main.appendChild(moveEl);
   });
 }
+
+function 평점별색상선택(vote_average) {
+  if (vote_average >= 8) {
+    return 'green';
+  } else if (vote_average >= 6) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
+// 검색창으로 검색했을때(이벤트)
+form.addEventListener('submit', (e) => {
+  //원래 form submit 이벤트는 서버로 입력 데이터를 보내는데 그 이벤트는 제거
+  e.preventDefault();
+  // 우리가 원하는 이벤트 실행
+  const searchTerm = search.value;
+  // console.log(searchTerm); //test
+  // searchTerm에 아무 내용이 없으면 null값이고 false임
+  if (searchTerm && searchTerm !== '') {
+    // 입력 내용이 공백이 아닐 경우 처리
+    getMovies(SEARCH_API + searchTerm);
+    search.value = '';
+  } else {
+    // 입력이 잘못됐을때
+    window.location.reload(); // 새로고침
+  }
+});
